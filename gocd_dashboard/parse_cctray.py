0ed2e08d421e5babfa3784bd105134156d463868
magnusl <magnus.lycka@pagero.com>
2015-11-26 23:27:35 +0100

    Parsing more complete (missing messages/blame)

--- gocd_dashboard/parse_cctray.py
+++ gocd_dashboard/parse_cctray.py
@@ -5,41 +5,95 @@ from xml.etree import ElementTree as Et
 class Projects(object):
     def __init__(self, fn):
         self.tree = Et.parse(fn)
-        self.activities = defaultdict(list)
         self.pipelines = {}
         self.stages = defaultdict(list)
         self.jobs = defaultdict(list)
-        self.stages = defaultdict(list)
         self.parse()
 
     def parse(self):
         for project in self.tree.findall('Project'):
-            self.activities[project.attrib['activity']].append(project)
             name_parts = [n.strip() for n in project.attrib['name'].split('::')]
-            pipeline_name = name_parts[0]
-            stage_name = name_parts[1]
+            project.pipeline_name = name_parts[0]
+            project.stage_name = name_parts[1]
             if len(name_parts) > 2:
-                job_name = name_parts[2]
+                project.job_name = name_parts[2]
             else:
-                job_name = None
-            if pipeline_name not in self.pipelines:
-                self.pipelines[pipeline_name] = Pipeline()
-            self.pipelines[pipeline_name].add_facts(project)
-            if len(name_parts) == 2:
-                self.stages[pipeline_name].append(stage_name)
-            if len(name_parts) == 3:
-                self.jobs[(pipeline_name, stage_name)].append(job_name)
+                project.job_name = None
+            if project.pipeline_name not in self.pipelines:
+                self.pipelines[project.pipeline_name] = Pipeline()
+            self.pipelines[project.pipeline_name].add_facts(project)
 
+    @staticmethod
+    def green_which(entity):
+        return True
+
+    @staticmethod
+    def yellow_which(entity):
+        return entity.status != 'Success'
+
+    @staticmethod
+    def red_which(entity):
+        return 'Failure' in entity.status
+
+    @staticmethod
+    def time_key(enity):
+        return enity.changed
+
+    def select(self, which):
+        selection = {
+            'green': self.green_which,
+            'yellow': self.yellow_which,
+            'red': self.red_which,
+        }[which]
+        pipelines = [p for p in self.pipelines.values() if selection(p)]
+        pipelines.sort(key=self.time_key)
+        pipelines.reverse()
+        return pipelines
 
 class Pipeline(object):
     def __init__(self):
         self.activity_set = set()
         self.last_build_set = set()
         self.status = None
+        self.changed = None
+        self.url = None
+        self.name = None
+        self.stages = []
 
     def add_facts(self, project):
+        self.changed = max(self.changed, project.attrib['lastBuildTime'])
+        if not self.name:
+            self.name = project.pipeline_name
+            self.url = project.attrib['webUrl'].rsplit('/',2)[0]
+        if not project.stage_name in [stage.name for stage in self.stages]:
+            self.stages.append(Stage(project))
+        if project.job_name:
+            self.stages[-1].add_job(project)
         self.activity_set.add(project.attrib['activity'])
         self.last_build_set.add(project.attrib['lastBuildStatus'])
         prefix = "Building after " if "Building" in self.activity_set else ""
         suffix = "Failure" if "Failure" in self.last_build_set else "Success"
         self.status = prefix + suffix
+
+
+class Entity(object):
+    def __init__(self, project):
+        prefix = "Building after " if project.attrib['activity'] == "Building" else ""
+        self.status = prefix + project.attrib['lastBuildStatus']
+        self.url = project.attrib['webUrl']
+
+
+class Stage(Entity):
+    def __init__(self, project):
+        super(Stage, self).__init__(project)
+        self.jobs = []
+        self.name = project.stage_name
+
+    def add_job(self, project):
+        self.jobs.append(Job(project))
+
+class Job(Entity):
+    def __init__(self, project):
+        super(Job, self).__init__(project)
+        self.name = project.job_name
+
--- test/test_parse_cctray.py
+++ test/test_parse_cctray.py
@@ -2,44 +2,73 @@ import unittest
 from parse_cctray import Projects
 
 
-class TestParseProjects(unittest.TestCase):
+class TestPipelines(unittest.TestCase):
     def setUp(self):
         self.projects = Projects('data/cctray.xml')
-
-    def test_count_projects(self):
-        self.assertEqual(len(self.projects.tree.findall('Project')), 23)
-
-    def test_activity_count(self):
-        activities = {
-            'Sleeping': 19,
-            'Building': 4,
-        }
-        for activity_type, count in activities.items():
-            self.assertEqual(len(self.projects.activities[activity_type]), count)
+        self.pipelines = self.projects.pipelines
 
     def test_count_pipelines(self):
-        self.assertEqual(len(self.projects.pipelines), 9)
+        self.assertEqual(9, len(self.projects.pipelines))
 
-    def test_count_stages(self):
-        self.assertEqual(len(self.projects.stages['snafu-service']), 2)
-
-    def test_count_jobs(self):
-        self.assertEqual(len(self.projects.jobs[('snafu-service', 'test')]), 2)
-
-    def test_pipeline_staus(self):
+    def test_pipeline_attributes(self):
         expected = {
-            "foo": "Success",
-            "bar": "Failure",
-            "baz": "Failure",
-            "qux": "Building after Failure",
-            "norf": "Building after Success",
-            "large-norf": "Failure",
-            "fnord": "Success",
-            "snafu-service": "Success",
-            "snafu-transformation": "Success",
+            "foo": ("Success", "2015-11-26T11:45:17", "http://go-server/go/pipelines/foo/382"),
+            "bar": ("Failure", "2015-11-03T09:16:23", "http://go-server/go/pipelines/bar/57"),
+            "baz": ("Failure", "2015-11-26T11:46:33", "http://go-server/go/pipelines/baz/203"),
+            "qux": ("Building after Failure", "2015-11-26T12:29:28", "http://go-server/go/pipelines/qux/240"),
+            "norf": ("Building after Success", "2015-11-26T12:10:11", "http://go-server/go/pipelines/norf/190"),
+            "large-norf": ("Failure", "2015-11-20T15:44:54", "http://go-server/go/pipelines/large-norf/13"),
+            "fnord": ("Success", "2015-11-26T13:15:09", "http://go-server/go/pipelines/fnord/98"),
+            "snafu-service": ("Success", "2015-11-26T11:28:42", "http://go-server/go/pipelines/snafu-service/250"),
+            "snafu-transformation": ("Success", "2015-11-25T14:37:04", "http://go-server/go/pipelines/snafu-transformation/100"),
         }
-        for name, pipeline in self.projects.pipelines.items():
-            self.assertEqual(pipeline.status, expected[name])
+        for name, pipeline in self.pipelines.items():
+            self.assertEqual(name, pipeline.name)
+            self.assertEqual(expected[name][0], pipeline.status)
+            self.assertEqual(expected[name][1], pipeline.changed)
+            self.assertEqual(expected[name][2], pipeline.url)
+
+    def test_pipeline_selection_green(self):
+        expected = "fnord qux norf baz foo snafu-service snafu-transformation large-norf bar".split()
+        self.assertEqual(expected, [p.name for p in self.projects.select('green')])
+
+    def test_pipeline_selection_yellow(self):
+        expected = "qux norf baz large-norf bar".split()
+        self.assertEqual(expected, [p.name for p in self.projects.select('yellow')])
+
+    def test_pipeline_selection_red(self):
+        expected = "qux baz large-norf bar".split()
+        self.assertEqual(expected, [p.name for p in self.projects.select('red')])
+
+
+class TestStages(unittest.TestCase):
+    def setUp(self):
+        self.stages = Projects(open('data/cctray.xml')).pipelines['baz'].stages
+
+    def test_pipeline_stages(self):
+        self.assertEqual(2, len(self.stages))
+        self.assertEqual('gamma', self.stages[0].name)
+        self.assertEqual('Success', self.stages[0].status)
+        self.assertEqual('http://go-server/go/pipelines/baz/203/readyToTest/1',
+                         self.stages[0].url)
+        self.assertEqual('delta', self.stages[1].name)
+        self.assertEqual('Failure', self.stages[1].status)
+
+
+class TestJobs(unittest.TestCase):
+    def setUp(self):
+        self.jobs = Projects(open('data/cctray.xml')).pipelines['snafu-service'].stages[1].jobs
+
+    def test_stage_jobs(self):
+        self.assertEqual(2, len(self.jobs))
+        self.assertEqual('Integration_Tests', self.jobs[0].name)
+        self.assertEqual('Success', self.jobs[0].status)
+        self.assertEqual("http://go-server/go/tab/build/detail/snafu-service/250/test/1/Integration_Tests",
+                         self.jobs[0].url)
+        self.assertEqual('DB_Tests', self.jobs[1].name)
+        self.assertEqual('Success', self.jobs[1].status)
+        self.assertEqual("http://go-server/go/tab/build/detail/snafu-service/250/test/1/DB_Tests",
+                         self.jobs[1].url)
 
 
 if __name__ == '__main__':
