4e468210f6c2e61dc0946e877aeed549796c1f71
Wilhelm Hedman <wilhelm.hedman@pagero.com>
2016-07-13 16:24:06 +0200

    Added sanitized unit tests

---
+++ test/test_read_config.py
@@ -0,0 +1,38 @@
+import unittest
+from dash_board import read_config
+
+
+def get_max_mock(name):
+    if name == "characterize":
+        return 1800, 1800
+    else:
+        return 2200, 2200
+
+
+class TestReadFile(unittest.TestCase):
+    def testRegularConfig(self):
+        input = dict()
+        input["pipelines"] = [
+            {"name": "characterize", "begin_at": 1500},
+            {"name": "feature", "begin_at": 100},
+        ]
+
+        pipelines = read_config.parse_config(input)
+        self.assertEqual(pipelines, [("characterize", 1500), ("feature", 100)])
+
+
+
+    def testNoBeginAt(self):
+        saved = read_config.get_max_pipeline_status
+        read_config.get_max_pipeline_status = get_max_mock
+
+        input = dict()
+        input["pipelines"] = [
+            {"name": "characterize", "begin_at": 1500},
+            {"name": "feature"},
+        ]
+
+        pipelines = read_config.parse_config(input)
+        self.assertEqual(pipelines, [("characterize", 1500), ("feature", 1800)])
+
+        read_config.get_max_pipeline_status = saved
