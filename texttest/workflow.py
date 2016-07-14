103a178dba1de2c44519fe293a05c787b9ef39ae
Elias Djurfeldt <elias.djurfeldt@pagero.com>
2016-07-14 16:35:18 +0200

    Added log writing for urls in urls.txt in texttest.

--- texttest/workflow.py
+++ texttest/workflow.py
@@ -1,12 +1,15 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+
+import subprocess
 
 from docker_management import ContainerManager
+
 
 def start_servers(docker):
     # start db docker on unique port
     # load data into db from flat files in test case
     # start the dashboard server on unique port
-    db_port = 15554 # TODO: make the db port configurable in data_access.py so it can be set on the command line
+    db_port = 15550 # TODO: make the db port configurable in data_access.py so it can be set on the command line
     db_container = _start_db_docker(docker, db_port)
 
     application_port = 5000
@@ -28,7 +31,9 @@ def perform_testcase():
                 # navigate to the url
                 # make ascii art of the page
                 # store the ascii art in the 'gui_log.txt' file
-                log.write("store the ascii art here")
+                sprocess_call = subprocess.Popen(["/usr/bin/lynx", "-dump", url], stdout=subprocess.PIPE)
+                out = sprocess_call.communicate()[0]
+                log.write(out)
 
 def main():
     docker = ContainerManager("dockerregistry.pagero.local") # TODO: Put this image on docker hub instead of our internal docker registry
