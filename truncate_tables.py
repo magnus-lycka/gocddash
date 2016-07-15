070a7a63634fabceff60c92f56b5f81bf81ec621
Elias Djurfeldt <elias.djurfeldt@pagero.com>
2016-07-15 11:26:09 +0200

    Fixed more imports.

--- truncate_tables.py
+++ truncate_tables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from gocddash.analysis import truncate_tables
+from gocddash.analysis.data_access import truncate_tables
 
 if __name__ == '__main__':
     truncate_tables()
