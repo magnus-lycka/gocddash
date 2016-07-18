834619c832208b7bc8a99ff3d673c5b50e348cba
Wilhelm Hedman <wilhelm.hedman@pagero.com>
2016-07-18 10:20:11 +0200

    Refactored go_client

--- gocddash/analysis/go_client.py
+++ gocddash/analysis/go_client.py
@@ -8,48 +8,51 @@ class GoSource:
         self.base_go_url = base_go_url
         self.api_go_url = base_go_url + "api/"
         self.auth = auth
-    
+
+    def api_request(self, url, **kwargs):
+        return self.base_request("api/" + url, **kwargs)
+
+    def base_request(self, url, **kwargs):
+        # TODO: get back the headers for agent fetching
+        return requests.get(self.base_go_url + url, auth=self.auth)
+
     def go_request_pipeline_history(self, pipeline_name, offset=0):
-        request = requests.get(self.api_go_url + "pipelines/" + pipeline_name + "/history/" + str(offset), auth=self.auth)
-        return request
+        return self.api_request("pipelines/" + pipeline_name + "/history/" + str(offset))
 
     def go_get_pipeline_instance(self, pipeline_name, pipeline_counter):
-        return requests.get(self.api_go_url + "pipelines/" + pipeline_name + "/instance/" + str(pipeline_counter) + "/",
-                            auth=self.auth).content
+        return self.api_request("pipelines/" + pipeline_name + "/instance/" + str(pipeline_counter) + "/").content
 
     def go_get_stage_instance(self, pipeline_name, pipeline_counter, stage_name):
-        return requests.get(
-            self.api_go_url + "stages/" + pipeline_name + "/" + stage_name + "/instance/" + str(pipeline_counter) + "/1",
-            auth=self.auth).content
+        return self.api_request(
+            "stages/" + pipeline_name + "/" + stage_name + "/instance/" + str(pipeline_counter) + "/1").content
 
     def go_request_stages_history(self, pipeline_name, pipeline_id, stage, stage_name):
-        return requests.get(
-            self.api_go_url + "stages/" + pipeline_name + "/" + stage_name + "/instance/" + str(pipeline_id) + "/" + str(
-                stage),
-            auth=self.auth).content
+        return self.api_request(
+            "stages/" + pipeline_name + "/" + stage_name + "/instance/" + str(pipeline_id) + "/" + str(stage)).content
 
     def go_get_agent_information(self, agent_uuid):
-        return requests.get(self.api_go_url + "agents/" + agent_uuid, auth=self.auth,
-                            headers={"Accept": "application/vnd.go.cd.v2+json"})
-
-    def go_request_junit_report(self, pipeline_name, pipeline_id, stage, stage_name):
-        return requests.get(self.base_go_url + "files/" + pipeline_name + "/" + str(pipeline_id)
-                            + "/" + stage_name + "/" + str(stage) + "/defaultJob/testoutput/index.html",
-                            auth=self.auth).content.decode("utf-8")
+        return self.api_request("agents/" + agent_uuid, headers={"Accept": "application/vnd.go.cd.v2+json"})
 
     def go_request_job_history(self, pipeline_name, stage_name, offset=0):
-        return requests.get(
-            self.api_go_url + "jobs/" + pipeline_name + "/" + stage_name + "/defaultJob/history/" + str(offset),
-            auth=self.auth).content
+        return self.api_request(
+            "jobs/" + pipeline_name + "/" + stage_name + "/defaultJob/history/" + str(offset)).content
+
+    def go_request_junit_report(self, pipeline_name, pipeline_id, stage, stage_name):
+        return self.base_request("files/" + pipeline_name + "/" + str(pipeline_id)
+                                 + "/" + stage_name + "/" + str(
+            stage) + "/defaultJob/testoutput/index.html").content.decode("utf-8")
 
     def go_request_console_log(self, pipeline_name, pipeline_id, stage_index, stage_name):
-        return requests.get(self.base_go_url + "files/" + pipeline_name + "/" + str(pipeline_id)
-                            + "/" + stage_name + "/" + str(stage_index) + "/defaultJob/cruise-output/console.log",
-                            auth=self.auth).content.decode("utf-8")
+        return self.base_request("files/" + pipeline_name + "/" + str(pipeline_id)
+                                 + "/" + stage_name + "/" + str(
+            stage_index) + "/defaultJob/cruise-output/console.log").content.decode("utf-8")
 
     def go_request_comparison_html(self, pipeline_name, current, comparison):
-        return requests.get(self.base_go_url + "compare/{}/{}/with/{}".format(pipeline_name, current, comparison),
-                            auth=self.auth).content.decode('utf-8', 'ignore')
+        return self.base_request("compare/{}/{}/with/{}".format(pipeline_name, current, comparison)).content.decode(
+            'utf-8', 'ignore')
+
+    def go_get_cctray(self):
+        return self.base_request(self.base_go_url + "cctray.xml").content.decode('utf-8', 'ignore')
 
 
 class FileSource:
@@ -83,6 +86,8 @@ class FileSource:
     def go_request_comparison_html(self, pipeline_name, current, comparison):
         return ""
 
+    def go_get_cctray(self):
+        return ""
 
 source = GoSource(PipelineConfig().get_base_go_url(), PipelineConfig().get_auth())
 
