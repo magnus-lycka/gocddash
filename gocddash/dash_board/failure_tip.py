c6070318e54a6b2c643350d7aa7221f5b39e2deb
Elias Djurfeldt <elias.djurfeldt@pagero.com>
2016-07-22 16:12:50 +0200

    Refined failure tips to be more intelligent. Fixed if statement in Insights for Result vs Status display.

--- gocddash/dash_board/failure_tip.py
+++ gocddash/dash_board/failure_tip.py
@@ -6,8 +6,11 @@ def get_failure_tip(current, previous, last_success):
 
 
 def handle_failure(current, previous, last_success):
+    desc = ""
+    if previous.stage.description and previous.stage.responsible:
+        desc = "Last claim: {}: {}".format(previous.stage.responsible, previous.stage.description)
     if current.stage.failure_stage == "POST":
-        return "All tests passed, but the build failed."
+        return "All tests passed, but the build failed. {}".format(desc)
     elif current.stage.failure_stage == "STARTUP":
         return "Tests failed during STARTUP."
     else:
@@ -19,12 +22,12 @@ def handle_test_failure(current, previous, last_success):
     failure_recommendation = FailureRecommendation(current, previous, last_success)
     if failure_recommendation.initial_tip_available():
 
-        if len(current.test_names) == 1:
-            return "Only one test failure. Potential for flickering."
-
         initial_tip = failure_recommendation.get_initial_recommendation()
         if initial_tip:
             return initial_tip
+
+    if len(current.test_names) == 1:
+        return "Only one test failure. Potential for flickering."
 
     fallback_tip = failure_recommendation.get_fallback_recommendation()
 
@@ -36,32 +39,39 @@ class FailureRecommendation:
         self.current = current
         self.previous = previous
         self.last_success = last_success
+        desc = ""
+        if previous.stage.description and previous.stage.responsible:
+            desc = "Last claim: {}: {}".format(previous.stage.responsible, previous.stage.description)
 
         # This is a bit hard to read
         self.statistics_actions = [
             (
+                lambda: self.current.stage.failure_stage == "STARTUP" and self.current.stage.failure_stage == self.previous.stage.failure_stage,
+                "Tests have failed at STARTUP several times in a row. Recommend to rerun after investigation. {}".format(desc)),
+            (
                 lambda: len(self.current.test_names) == 1 and 1 in current.test_names,  # Very specific for characterize
-                "Flickering"),
+                "Flickering. Recommend to rerun."),
             (
                 lambda: self.current.failure_signature == self.previous.failure_signature and self.current.test_names == self.previous.test_names,
-                "Same failure signature and same failure indices. Not flickering."),
+                "Same failure signature and same failure indices as last failure. {}".format(desc)),
             (lambda: self.current.failure_signature == self.previous.failure_signature,
-             "Same failure signature as last test. Unlikely flickering."),
+             "Same failure signature as last test. Unlikely flickering. {}".format(desc)),
             (lambda: self.current.test_names == self.previous.test_names,
-             "Same failure indices as last test. Unlikely flickering."),
+             "Same failure indices as last test. Unlikely flickering. {}".format(desc)),
             (lambda: len(current.test_names) == 1,
-             "Only one test failure. Potential for flickering but needs further investigation."),
+             "Only one test failure. Potential for flickering but needs further investigation. {}".format(desc)),
             (lambda: len(self.current.test_names) == len(self.previous.test_names),
-             "Same number of failures as previous test. Unlikely flickering.")
+             "Same number of failures as previous test. Unlikely flickering. {}".format(desc))
         ]
 
         self.fallback_actions = [
-            (lambda: self.previous.is_success(), "Last pipeline was a success. Potential for flickering but needs further investigation."),
+            (lambda: self.previous.is_success(),
+             "Last pipeline was a success. Potential for flickering but needs further investigation. {}".format(desc)),
             (lambda: self.current.stage.pipeline_counter - last_success >= 3,
-             "Pipeline hasn't been green in a long time. Not flickering."),
+             "Pipeline hasn't been green in a long time. Not flickering. {}".format(desc)),
         ]
 
-        self.default = "Last pipeline was a failure. Potential for flickering but more likely a real failure."
+        self.default = "Last pipeline was a failure. {}".format(desc)
 
     def get_initial_recommendation(self):
         statistics_match = self.any_match()
