0eafb85f151bcac08a360a8f6928a7b29b253be7
Wilhelm Hedman <wilhelm.hedman@pagero.com>
2016-07-25 10:51:00 +0200

    Many results in graph statistics!

--- gocddash/analysis/domain.py
+++ gocddash/analysis/domain.py
@@ -113,6 +113,6 @@ class GraphData:
 def get_graph_data(pipeline_name):
     result = get_connection().get_graph_data(pipeline_name)
     if result:
-        return GraphData(*result)
+        return list(map(lambda gd: GraphData(*result), result))
     else:
         return None
