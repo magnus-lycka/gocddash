4782b1975fcb1fb8e001c805975fcc2dc1c9fe6c
Wilhelm Hedman <wilhelm.hedman@pagero.com>
2016-08-11 09:20:10 +0200

    Correct order for dependent views

--- migrations/0002.create-views.py
+++ migrations/0002.create-views.py
@@ -4,6 +4,16 @@
 from yoyo import step
 #
 steps = [
+    step("""CREATE VIEW final_stages AS
+            SELECT s.*
+            FROM stage s
+            JOIN
+            (SELECT instance_id, max(stage_counter) as stage_counter, name
+            FROM stage
+            GROUP BY instance_id, name) sa
+            ON s.instance_id = sa.instance_id AND s.stage_counter = sa.stage_counter AND s.name = sa.name;""",
+         "DROP VIEW final_stages;"),
+
     step("""CREATE VIEW active_claims AS
             SELECT i.*
             FROM instance_claim i
@@ -12,7 +22,7 @@ steps = [
             JOIN pipeline_instance p ON p.pipeline_name = i.pipeline_name AND p.pipeline_counter = i.pipeline_counter
             JOIN final_stages s ON s.instance_id = p.id
             WHERE result <> 'Passed';""",
-         "DROP VIEW latest_claims;"),
+         "DROP VIEW active_claims;"),
 
     step("""CREATE VIEW failure_info AS
             SELECT p.pipeline_name, p.pipeline_counter, s.stage_counter, s.id, s.name as stage_name, p.trigger_message, s.approved_by, s.result, f.failure_stage, ac.responsible, ac.description, s.scheduled_date
@@ -32,16 +42,6 @@ steps = [
             JOIN agent a ON a.id = j.agent_uuid
             LEFT JOIN failure_information f ON f.stage_id = s.id;""",
          "DROP VIEW graph_statistics;"),
-
-    step("""CREATE VIEW final_stages AS
-            SELECT s.*
-            FROM stage s
-            JOIN
-            (SELECT instance_id, max(stage_counter) as stage_counter, name
-            FROM stage
-            GROUP BY instance_id, name) sa
-            ON s.instance_id = sa.instance_id AND s.stage_counter = sa.stage_counter AND s.name = sa.name;""",
-         "DROP VIEW final_stages;"),
 
     step("""CREATE VIEW graph_statistics_final_stages AS
             SELECT p.pipeline_name, p.pipeline_counter, s.stage_counter, s.name as stage_name, s.result as stage_result, j.name as job_name, j.scheduled_date, j.result as job_result, f.failure_stage, a.agent_name, j.tests_run, j.tests_failed, j.tests_skipped
