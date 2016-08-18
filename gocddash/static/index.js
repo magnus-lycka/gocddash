a9afd859db3664b301288e61a128d2186c52f557
Elias Djurfeldt <elias.djurfeldt@pagero.com>
2016-08-18 14:14:40 +0200

    Removed ability to claim failing builds directly from the index page. Claims must now be done from the insights pagei.

--- gocddash/static/index.js
+++ gocddash/static/index.js
@@ -11,40 +11,10 @@ $(function () {
     }
 });
 
-var pipelineName = null;
-var pipelineCounter = null;
 var shouldReload = true;
 reload_in_a_minute();
 
 
-$('#myModal').on('show.bs.modal', function (event) {
-    console.log("No more reloads!");
-    var icon = $(event.relatedTarget);
-    pipelineName = icon.data('pipeline_name');
-    pipelineCounter = icon.data('pipeline_counter');
-    shouldReload = false;
-});
-
-$("#responsible").keypress(function (e) {
-    if(e.which === 13){
-        postPayload()
-    }
-});
-
-$("#description").keypress(function (e) {
-    if(e.which === 13){
-        postPayload()
-    }
-});
-
-
-function postPayload() {
-    var payload = $('#claimForm').serialize() + '&pipelineName=' + pipelineName + '&pipelineCounter=' + pipelineCounter;
-    ajaxPostClaim(payload);
-}
-
-
-$('#postClaim').on('keypress click', postPayload);
 
 $(function () {
     $(".pipeline-alert").fadeIn(1000);
