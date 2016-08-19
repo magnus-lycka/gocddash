0436662480dfca49bca8b0cb3823427aa7662fd4
Elias Djurfeldt <elias.djurfeldt@pagero.com>
2016-08-19 10:31:09 +0200

    Improved docs.

--- gocddash/dash_board/read_pipeline_config.py
+++ gocddash/dash_board/read_pipeline_config.py
@@ -2,6 +2,7 @@ from gocddash.analysis.go_request import get_max_pipeline_status
 
 
 def parse_config(json_config):
+    """ Parses pipelines.json to get the pipeline name and where to start syncing from """
     name_start_tuple_list = [(key['name'], key['begin_at']) if "begin_at" in key else (
         key['name'], max(0, get_max_pipeline_status(key['name'])[1] - 20)) for key in json_config['pipelines']]
 
