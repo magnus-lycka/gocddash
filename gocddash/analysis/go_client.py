f51a665ed1b59409517acd46c9708a5c05ca392d
Magnus Lyckå <magnusl@pagero.com>
2016-11-08 18:00:24 +0100

    fixed go_get_agents API

--- gocddash/analysis/go_client.py
+++ gocddash/analysis/go_client.py
@@ -98,7 +98,7 @@ class GoSource:  # pragma: no cover
 
     def go_get_agents(self):
         request = self.api_request("agents", headers={"Accept": "application/vnd.go.cd.v2+json"})
-        return request.status_code == 200, request.content.decode("utf-8")
+        return request.content.decode("utf-8")
 
     def go_request_job_history(self, pipeline_name, stage_name, offset=0):
         return self.simple_api_request("jobs/{}/{}/defaultJob/history/{}".format(
